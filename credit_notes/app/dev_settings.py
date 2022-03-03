28bf22edd29dd78a17322fcde623b0c29d33a770
WinnyTroy <kiraguwinnie@gmail.com>
2022-03-03 08:39:03 +0300

    Introduce fast api to make application executable

--- credit_notes/app/dev_settings.py
+++ credit_notes/app/dev_settings.py
@@ -1,3 +1,3 @@
-unleashed_api_key = "wxkI3HUObJvZGwvuqD2Q0bfa8cTL0hOZ8cSsEJqvvEVgANtAxKRRKAdDCvA6wiZbJVf1IAY4LmhrHB3VsTNw=="
-unleashed_api_id = "e82aef74-5d53-4ff1-9fa7-ec3ed7b3a4e6"
+unleashed_api_key = "unleashedapisecretkey=="
+unleashed_api_id = "unleashed-api-id-9fa7"
 unleashed_url = "https://api.unleashedsoftware.com/"
