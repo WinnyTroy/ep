c875364d5cd40ef47de117c1fbc89ab650ab7226
WinnyTroy <kiraguwinnie@gmail.com>
2022-03-17 14:36:45 +0300

    Log errors to Sentry

--- credit_notes/app/settings.py
+++ credit_notes/app/settings.py
@@ -9,9 +9,13 @@ class Settings(BaseSettings):
     app_port: int = 8000
     app_root_path="/dev/"
     debug: bool = True
+    # Sentry
     sentry_dsn: str = "https://76b647949c0e4db5a8a6c1ad7a809ed6@o1162014.ingest.sentry.io/6248725"
+    # Unleashed
     unleashed_api_key = "wxkI3HUObJvZGwvuqD2Q0bfa8cTL0hOZ8cSsEJqvvEVgANtAxKRRKAdDCvA6wiZbJVf1IAY4LmhrHB3VsTNw=="
     unleashed_api_id = "e82aef74-5d53-4ff1-9fa7-ec3ed7b3a4e6"
     unleashed_url = "https://api.unleashedsoftware.com/"
+    # Database
+    database_url = "sqlite:///./credit_notes_app.db"
 
 settings = Settings()
