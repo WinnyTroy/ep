aafac2802dc99aa79e4be2c028656a813eb88a6c
WinnyTroy <kiraguwinnie@gmail.com>
2022-03-23 13:23:18 +0300

    Include functionality to fetch existing client invoices from sunculture database. Handle exceptions in the case that a user does not exist on any table on the sunculture database

--- credit_notes/app/utils/credit_notes_tools.py
+++ credit_notes/app/utils/credit_notes_tools.py
@@ -2,9 +2,14 @@ import json
 import hmac
 import base64
 import hashlib
+import logging
 import requests
-from .common_vars import create_credit_note_request_obj
+from fastapi import HTTPException
 from credit_notes.app.settings import settings
+from .common_vars import create_credit_note_request_obj
+from ..utils.db_connections_utils import (get_saved_user,
+                                         query_user_existing_installations,
+                                         query_user_installation_records)
 
 # main function interacting with unleashed API
 def unleashed_api_get_request(url_base, url_param,
@@ -28,21 +33,19 @@ def unleashed_api_get_request(url_base, url_param,
             'Content-Type': 'application/json'
     }
     # perform request
-    if request_data:
-        unleashed_data = requests.request(request_method,
-                                          data=json.dumps(request_data),
-                                          url=url, headers=headers)
-        print(f'Credit Note created from unleashed >>>>>>>>>>> {unleashed_data}')
-    else:
-        unleashed_data = requests.request(request_method,
-                                          url=url,
-                                          headers=headers)
-        print(f'Credit Note created from unleashed >>>>>>>>>>> {unleashed_data}')
-        print(f'Credit Note created from unleashed >>>>>>>>>>> {json.loads(unleashed_data.content)}')
-    # convert json to dict
-    unleashed_json = json.loads(unleashed_data.content)
-    return unleashed_json
+    unleashed_data = requests.request(request_method,
+                                        data=json.dumps(request_data),
+                                        url=url, headers=headers)
 
+    if unleashed_data.status_code != 200:
+        error_message = unleashed_data.json()['description']
+        logging.error(f'Request failed with-{error_message}')
+        raise HTTPException(
+            status_code=unleashed_data.status_code,
+            detail=error_message)
+
+    unleashed_response = unleashed_data.json()
+    return unleashed_response
 
 def get_single_unleashed_customer_details(name):
     '''
@@ -54,7 +57,6 @@ def get_single_unleashed_customer_details(name):
                                                url_param,
                                                request_method="get")
     return unleashed_json
-
 
 def create_credit_note(client_code, product_code,
                        warehouse_code, credit_amount, credit_reason):
@@ -80,3 +82,51 @@ def create_credit_note(client_code, product_code,
                                                            request_data)
 
     return unleashed_credit_note_json
+
+def calculate_existing_credits(db_user_installations):
+    '''
+    Function that sums up all existing credit records.
+    '''
+    # Handle scenerio where user had
+    # no existing installation records
+    if len(db_user_installations) > 1:
+        aggregated_credit_sum = sum(
+            [item.item_price for item in db_user_installations])
+    else:
+        aggregated_credit_sum = 0
+    return aggregated_credit_sum
+
+def fetch_client_existing_interest_invoices(client_id):
+    '''
+    Function that fetches existing credit notes
+    for specific client.
+    '''
+    # Fetch user account from db
+    import ipdb; ipdb.set_trace()
+    db_user_account = get_saved_user(client_id=client_id)
+
+    if not isinstance(db_user_account, HTTPException):
+        db_user_installations = query_user_installation_records(
+            db_user_account_id=db_user_account)
+        # Query financing component installations for current user
+        if not isinstance(db_user_installations, HTTPException):
+            db_user_existing_credit_invoices = query_user_existing_installations(
+                db_user_installations=db_user_installations)
+            return db_user_existing_credit_invoices
+        else:
+            return db_user_installations
+    else:
+        return db_user_account
+
+def aggregate_existing_credit(db_user_existing_credit_invoices, credit_amount):
+    # Instantiate credit value to 0
+    existing_client_credit_value = 0
+
+    # Determine if we should generate credit note
+    aggregated_existing_credits = calculate_existing_credits(
+        db_user_existing_credit_invoices)
+
+    # Deduct existing credit from credit amount requested
+    existing_client_credit_value = credit_amount - aggregated_existing_credits
+
+    return existing_client_credit_value
